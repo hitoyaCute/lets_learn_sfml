a60dcc9fb20f49964f287302fc3568ab47971512
hitoya <nokimberly74@gmail.com>
2025-10-21 18:48:03 +0800

    update

--- calculator/src/app/config.hpp
+++ calculator/src/app/config.hpp
@@ -1,3 +1,23 @@
+#include <SFML/System/Vector2.hpp>
+
+
+
+namespace conf {
+
+
+sf::Vector2u const window_size = {1280,800};
+sf::Vector2f const window_size_f = {window_size};
+
+
+
+
+
+
+
+
+}; // namespace conf
+
+
 
 
 
--- calculator/src/app/main.cpp
+++ calculator/src/app/main.cpp
@@ -1,9 +1,30 @@
 #include <SFML/Graphics/RenderWindow.hpp>
-
+#include <SFML/Window/WindowEnums.hpp>
+#include "config.hpp"
+#include "ui/event/events.hpp"
 
 
 int main() {
-  
+    sf::RenderWindow window{sf::VideoMode(conf::window_size), "Calculator", sf::State::Fullscreen};
+
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+
+    while (window.isOpen()) {
+        
+        Calc::process_event(window);
+
+
+
+
+
+        
+    }
+
+
+
+
+
 }
 
 
