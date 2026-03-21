8c7152b19bf68b8da21969b0ed50b60b5767809e
hitoya <nokimberly74@gmail.com>
2026-03-21 14:48:39 +0800

    first project
    travel trough the vast infinite stars, first project i create following pezzza's works (YT)

---
+++ stars/src/event.cpp
@@ -0,0 +1,15 @@
+#include "events.hpp"
+#include <SFML/Window/Keyboard.hpp>
+
+void process_event(sf::Window& window){
+  while (const std::optional event = window.pollEvent()) {
+    if (event->is<sf::Event::Closed>()) {
+      window.close();
+    }
+        if (const auto* key = event->getIf<sf::Event::KeyPressed>()) {
+      if (key->scancode == sf::Keyboard::Scancode::Escape){
+        window.close();
+      }
+    }
+  }
+}
---
+++ stars/src/star.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include <SFML/System/Vector2.hpp>
+struct Star {
+  sf::Vector2f position;
+  // the z axis position
+  float depth = 1.f;
+};
+
+
+
+
